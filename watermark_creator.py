32fc54c37ebc17288cc4f22d2f2954c2ccf91d1b
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2022-02-17 16:57:36 -0600

    Adding files

--- watermark_creator.py
+++ watermark_creator.py
@@ -1,15 +1,24 @@
 from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+import os
+import urllib.request
 
 # Grab watermark
-wm = Image.open("watermark.jpg")
+urllib.request.urlretrieve('https://www.dropbox.com/s/yiukpingxe55pqi/watermark.jpg?dl=1', 'wm.png')
+wm = Image.open('wm.png')
 wm_w, wm_h = wm.size
 
-# Grab picture
-p = Image.open("images/image2.jpg")
-p_w, p_h = p.size
-#print(w,h)
-offset = ((p_w - wm_w), (p_h - wm_h))
-p.paste(wm, offset)
-p.save('out.png')
+# Grab each image
+valid_exts = ['.jpeg', '.jpg', '.png']
+
+for filename in os.listdir("."):
+    f = os.path.join(filename)
+    # checking if it is an image
+    if os.path.isfile(f) and os.path.splitext(f)[1] in valid_exts:
+        p = Image.open(f)
+        p_w, p_h = p.size
+        offset = ((p_w - wm_w), (p_h - wm_h))
+        p.paste(wm, offset)
+        p.save(os.path.basename(f))
+
+# Remove wm image from dir
+os.remove('wm.png')
